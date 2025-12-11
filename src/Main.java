dfd6469dbc9f7e30315e3b550541e2f5c566473b
Daniel <tvland140@gmail.com>
2025-12-11 11:24:05 -0500

    PRIMERA LECTURA DE ARCHIVOS FUNCIONAL

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         //XML PARA PROBAR EN EL MAIN
         //<fruits>
         //    <fruit>apple</fruit>
@@ -53,11 +53,12 @@ public class Main {
         arbol.PostOrden(arbol.getRaiz());
 
         // ------ PROBAR BUSQUEDA POR ETIQUETA --------------
-        System.out.println("\n--- BUSQUEDA: 'fruit' ---");
-        for (NodoXML nodo : arbol.buscarPorEtiqueta("fruit")) {
-            System.out.println(nodo);
-        }
+        System.out.println("\n--- BUSQUEDA ORDENADA: 'fruit' ---");
+        List<String> valoresOrdenados = arbol.buscarValoresOrdenados("fruit");
 
+        for (String valor : valoresOrdenados) {
+            System.out.println(valor);
+        }
         // ------- PROBAR ORDENAMIENTO POR FRAGMENTO ---------
         System.out.println("\n--- ORDENAR POR FRAGMENTO 'berry' ---");
         List<NodoXML> ordenados = arbol.ordenarPorFragmento("fruit", "berry");
@@ -65,6 +66,122 @@ public class Main {
         for (NodoXML nodo : ordenados) {
             System.out.println(nodo);
         }
+*/
 
-    }
-}
+
+        /*
+        System.out.println("======= XML PARSER TEST =======");
+
+        //Crear el arbol
+        ArbolXML arbol = new ArbolXML();
+
+        // Archivo de prueba
+        String ruta = "src/archivo.xml";
+
+        // 1. Cargar XML
+        arbol.cargarXML(ruta);
+        System.out.println("\n[OK] Archivo XML cargado.\n");
+
+
+        // 2. Recorrido PREORDEN
+        System.out.println("=== PREORDEN ===");
+        arbol.preOrden(arbol.getRaiz());
+
+
+        // 3. Recorrido ENORDEN
+        System.out.println("\n=== ENORDEN ===");
+        arbol.enOrden(arbol.getRaiz());
+
+
+        // 4. Recorrido POSTORDEN
+        System.out.println("\n=== POSTORDEN ===");
+        arbol.PostOrden(arbol.getRaiz());
+
+
+        // 5. Buscar nodos por etiqueta
+        System.out.println("\n=== BUSCAR ETIQUETA 'name' ===");
+        List<NodoXML> listaName = arbol.buscarPorEtiqueta("name");
+        for (NodoXML nodo : listaName) {
+            System.out.println(nodo);
+        }
+
+
+        // 6. Buscar SOLO VALORES ordenados
+        System.out.println("\n=== VALORES ORDENADOS (Etiqueta: 'name') ===");
+        List<String> nombresOrdenados = arbol.buscarValoresOrdenados("name");
+        for (String s : nombresOrdenados) {
+            System.out.println(s);
+        }
+
+
+        // 7. Ordenar nodos por fragmento usando tu HEAP
+        System.out.println("\n=== ORDENAR NODOS QUE CONTIENEN 'wire' ===");
+        List<NodoXML> listaWire = arbol.ordenarPorFragmento("name", "wire");
+        for (NodoXML nodo : listaWire) {
+            System.out.println(nodo);
+        }
+
+
+        System.out.println("\n======= FIN DE PRUEBAS =======");
+
+*/
+        System.out.println("======= XML PARSER TEST 2 de archivito =======");
+
+        //Crear el arbol
+        ArbolXML arbol = new ArbolXML();
+
+        // Archivo de prueba
+        String ruta = "src/archivito.xml";
+
+        // 1. Cargar XML
+        arbol.cargarXML(ruta);
+        System.out.println("\n[OK] Archivo XML cargado.\n");
+
+
+        // 2. Recorrido PREORDEN
+        System.out.println("=== PREORDEN ===");
+        arbol.preOrden(arbol.getRaiz());
+
+
+        // 3. Recorrido ENORDEN
+        System.out.println("\n=== ENORDEN ===");
+        arbol.enOrden(arbol.getRaiz());
+
+
+        // 4. Recorrido POSTORDEN
+        System.out.println("\n=== POSTORDEN ===");
+        arbol.PostOrden(arbol.getRaiz());
+
+
+        // 5. Buscar nodos por etiqueta
+        System.out.println("\n=== BUSCAR ETIQUETA 'name' ===");
+        List<NodoXML> listaName = arbol.buscarPorEtiqueta("color");
+        for (NodoXML nodo : listaName) {
+            System.out.println(nodo);
+        }
+
+
+        // 6. Buscar SOLO VALORES ordenados
+        System.out.println("\n=== VALORES ORDENADOS (Etiqueta: 'name') ===");
+        List<String> nombresOrdenados = arbol.buscarValoresOrdenados("color");
+        for (String s : nombresOrdenados) {
+            System.out.println(s);
+        }
+
+
+        // 7. Ordenar nodos por fragmento usando tu HEAP
+        System.out.println("\n=== ORDENAR NODOS QUE CONTIENEN 'wire' ===");
+        List<NodoXML> listaWire = arbol.ordenarPorFragmento("name", "op");
+        for (NodoXML nodo : listaWire) {
+            System.out.println(nodo);
+        }
+
+
+        System.out.println("\n======= FIN DE PRUEBAS =======");
+
+
+
+
+            }
+        }
+
